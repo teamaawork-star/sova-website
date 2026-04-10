a0cc60e6f91438e5c34cd682212adfa6c3a360e6
teamaawork-star <teamaa.work@gmail.com>
2026-04-10 15:58:11 +0300

    timeweb1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- Добавьте этот импорт
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- Добавьте этот плагин
+    tailwindcss(),
   ],
+  base: './', // <-- Обязательно добавляем эту строку для Timeweb
 })
